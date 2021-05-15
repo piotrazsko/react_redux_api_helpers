bb9f8db91f098bfd429c418d71920a8c2df64424
Petrashka <serj42@yandex.ru>
2021-05-15 13:11:43 +0300

    change error to warning

--- dist/modules/apiRoutes.js
+++ dist/modules/apiRoutes.js
@@ -1,26 +1,26 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-    value: true
+	value: true
 });
 var ApiRoutes = new function () {
-    var _this = this;
+	var _this = this;
 
-    var instance = this;
-    this.routes = {};
-    this.add = function (key, func) {
-        if (typeof func === 'function') {
-            if (key in _this.routes) {
-                throw new Error('Check your key! This key is already in use.');
-            }
-            _this.routes[key] = func;
-        } else {
-            throw new Error('Check your arguments');
-        }
-    };
-    return function () {
-        return instance;
-    };
+	var instance = this;
+	this.routes = {};
+	this.add = function (key, func) {
+		if (typeof func === 'function') {
+			if (key in _this.routes) {
+				console.warn('Check your key! This key is already in use.');
+			}
+			_this.routes[key] = func;
+		} else {
+			throw new Error('Check your arguments');
+		}
+	};
+	return function () {
+		return instance;
+	};
 }();
 var apiRoutes = exports.apiRoutes = new ApiRoutes();
 var _default = ApiRoutes;
@@ -28,15 +28,15 @@ exports.default = _default;
 ;
 
 var _temp = function () {
-    if (typeof __REACT_HOT_LOADER__ === 'undefined') {
-        return;
-    }
+	if (typeof __REACT_HOT_LOADER__ === 'undefined') {
+		return;
+	}
 
-    __REACT_HOT_LOADER__.register(ApiRoutes, 'ApiRoutes', 'src/modules/apiRoutes.js');
+	__REACT_HOT_LOADER__.register(ApiRoutes, 'ApiRoutes', 'src/modules/apiRoutes.js');
 
-    __REACT_HOT_LOADER__.register(apiRoutes, 'apiRoutes', 'src/modules/apiRoutes.js');
+	__REACT_HOT_LOADER__.register(apiRoutes, 'apiRoutes', 'src/modules/apiRoutes.js');
 
-    __REACT_HOT_LOADER__.register(_default, 'default', 'src/modules/apiRoutes.js');
+	__REACT_HOT_LOADER__.register(_default, 'default', 'src/modules/apiRoutes.js');
 }();
 
 ;
