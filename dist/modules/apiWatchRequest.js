deaee43b2d8bd2985fd39844ed73b07de2374bf4
Serj <serj432@yandex.ru>
2021-09-20 15:24:27 +0300

    changed  apiWatch falsed

--- dist/modules/apiWatchRequest.js
+++ dist/modules/apiWatchRequest.js
@@ -1,7 +1,7 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-    value: true
+	value: true
 });
 
 var _regenerator = require('babel-runtime/regenerator');
@@ -45,166 +45,166 @@ var _marked = /*#__PURE__*/_regenerator2.default.mark(callApi),
 
 var apiRoutes = new _apiRoutes2.default();
 function callApi(action, apiMethods, options) {
-    var defaultOptions, _options, additiveCallback, apiService, successCallback, failedCallback, stopRequest, preventSuccessAction, preventFailedAction, apiRequest, data, actionsTypes, response, errorModel;
+	var defaultOptions, _options, additiveCallback, apiService, successCallback, failedCallback, stopRequest, preventSuccessAction, preventFailedAction, apiRequest, data, actionsTypes, response, errorModel;
 
-    return _regenerator2.default.wrap(function callApi$(_context) {
-        while (1) {
-            switch (_context.prev = _context.next) {
-                case 0:
-                    defaultOptions = {
-                        apiService: _axios2.default,
-                        additiveCallback: null,
-                        successCallback: null,
-                        failedCallback: null,
-                        stopRequest: function stopRequest() {
-                            // callback  used before request -  we can stop it
-                            return false;
-                        },
-                        preventSuccessAction: false,
-                        preventFailedAction: false
-                    };
+	return _regenerator2.default.wrap(function callApi$(_context) {
+		while (1) {
+			switch (_context.prev = _context.next) {
+				case 0:
+					defaultOptions = {
+						apiService: _axios2.default,
+						additiveCallback: null,
+						successCallback: null,
+						failedCallback: null,
+						stopRequest: function stopRequest() {
+							// callback  used before request -  we can stop it
+							return false;
+						},
+						preventSuccessAction: false,
+						preventFailedAction: false
+					};
 
-                    options = (0, _extends3.default)({}, defaultOptions, options);
-                    /**
-                     * [additiveCallback used for prepare  request]
-                     * [stopRequest used for  stop responce if nesessary ]
-                     * [onSuccess used for  listening of result  - get from  action ]
-                     * [beforeRequestCallback used before  request- get from  action ]
-                     * [responseDataPrepare  used for prepare responce before save to store- get from  action ]
-                     * [preventSuccess  used for  flag for prevent save to store- get from  action ]
-                     * [preventSuccessAction  used for  flag for prevent save to store ]
-                     * [postSaveToStoreCallback  used after save to store- get from  action ]
-                     * @type {[type]}
-                     */
-                    _options = options, additiveCallback = _options.additiveCallback, apiService = _options.apiService, successCallback = _options.successCallback, failedCallback = _options.failedCallback, stopRequest = _options.stopRequest, preventSuccessAction = _options.preventSuccessAction, preventFailedAction = _options.preventFailedAction;
-                    apiRequest = apiMethods[action.type];
+					options = (0, _extends3.default)({}, defaultOptions, options);
+					/**
+      * [additiveCallback used for prepare  request]
+      * [stopRequest used for  stop responce if nesessary ]
+      * [onSuccess used for  listening of result  - get from  action ]
+      * [beforeRequestCallback used before  request- get from  action ]
+      * [responseDataPrepare  used for prepare responce before save to store- get from  action ]
+      * [preventSuccess  used for  flag for prevent save to store- get from  action ]
+      * [preventSuccessAction  used for  flag for prevent save to store ]
+      * [postSaveToStoreCallback  used after save to store- get from  action ]
+      * @type {[type]}
+      */
+					_options = options, additiveCallback = _options.additiveCallback, apiService = _options.apiService, successCallback = _options.successCallback, failedCallback = _options.failedCallback, stopRequest = _options.stopRequest, preventSuccessAction = _options.preventSuccessAction, preventFailedAction = _options.preventFailedAction;
+					apiRequest = apiMethods[action.type];
 
-                    if (!(typeof apiRequest === 'function')) {
-                        _context.next = 42;
-                        break;
-                    }
+					if (!(typeof apiRequest === 'function')) {
+						_context.next = 42;
+						break;
+					}
 
-                    data = apiRequest(action.payload);
+					data = apiRequest(action.payload);
 
-                    if (!(typeof additiveCallback === 'function')) {
-                        _context.next = 10;
-                        break;
-                    }
+					if (!(typeof additiveCallback === 'function')) {
+						_context.next = 10;
+						break;
+					}
 
-                    _context.next = 9;
-                    return additiveCallback(data);
+					_context.next = 9;
+					return additiveCallback(data);
 
-                case 9:
-                    data = _context.sent;
+				case 9:
+					data = _context.sent;
 
-                case 10:
-                    actionsTypes = (0, _helpers.responseActionsTypes)(action.type);
-                    _context.prev = 11;
+				case 10:
+					actionsTypes = (0, _helpers.responseActionsTypes)(action.type);
+					_context.prev = 11;
 
-                    if (stopRequest(data)) {
-                        _context.next = 28;
-                        break;
-                    }
+					if (stopRequest(data)) {
+						_context.next = 28;
+						break;
+					}
 
-                    if (typeof action.beforeRequestCallback === 'function') {
-                        action.beforeRequestCallback(data);
-                    }
-                    _context.next = 16;
-                    return (0, _effects.call)(apiService, {
-                        data: data
-                    });
+					if (typeof action.beforeRequestCallback === 'function') {
+						action.beforeRequestCallback(data);
+					}
+					_context.next = 16;
+					return (0, _effects.call)(apiService, {
+						data: data
+					});
 
-                case 16:
-                    response = _context.sent;
+				case 16:
+					response = _context.sent;
 
-                    if (typeof action.onSuccess === 'function') {
-                        action.onSuccess(response);
-                    }
+					if (typeof action.onSuccess === 'function') {
+						action.onSuccess(response);
+					}
 
-                    if (!(typeof successCallback === 'function')) {
-                        _context.next = 21;
-                        break;
-                    }
+					if (!(typeof successCallback === 'function')) {
+						_context.next = 21;
+						break;
+					}
 
-                    _context.next = 21;
-                    return (0, _effects.call)(successCallback, response);
+					_context.next = 21;
+					return (0, _effects.call)(successCallback, response);
 
-                case 21:
-                    if (typeof action.responseDataPrepare === 'function') {
-                        response = action.responseDataPrepare(response);
-                    }
+				case 21:
+					if (typeof action.responseDataPrepare === 'function') {
+						response = action.responseDataPrepare(response);
+					}
 
-                    if (!(!preventSuccessAction && !action.preventSuccess)) {
-                        _context.next = 25;
-                        break;
-                    }
+					if (!(!preventSuccessAction && !action.preventSuccess)) {
+						_context.next = 25;
+						break;
+					}
 
-                    _context.next = 25;
-                    return (0, _effects.put)({
-                        response: response,
-                        type: actionsTypes.successAction,
-                        payload: action.payload,
-                        key: action.key
-                    });
+					_context.next = 25;
+					return (0, _effects.put)({
+						response: response,
+						type: actionsTypes.successAction,
+						payload: action.payload,
+						key: action.key
+					});
 
-                case 25:
-                    if (!(typeof action.postSaveToStoreCallback === 'function')) {
-                        _context.next = 28;
-                        break;
-                    }
+				case 25:
+					if (!(typeof action.postSaveToStoreCallback === 'function')) {
+						_context.next = 28;
+						break;
+					}
 
-                    _context.next = 28;
-                    return (0, _effects.call)([action, 'postSaveToStoreCallback'], response);
+					_context.next = 28;
+					return (0, _effects.call)([action, 'postSaveToStoreCallback'], response);
 
-                case 28:
-                    _context.next = 40;
-                    break;
+				case 28:
+					_context.next = 40;
+					break;
 
-                case 30:
-                    _context.prev = 30;
-                    _context.t0 = _context['catch'](11);
-                    errorModel = {
-                        type: actionsTypes.failedAction,
-                        payload: action.payload,
-                        message: _context.t0.statusText,
-                        status: _context.t0.status,
-                        response: _context.t0.response
-                    };
+				case 30:
+					_context.prev = 30;
+					_context.t0 = _context['catch'](11);
+					errorModel = {
+						type: actionsTypes.failedAction,
+						payload: action.payload,
+						message: _context.t0.statusText,
+						status: _context.t0.status,
+						response: _context.t0.response
+					};
 
-                    if (typeof action.onFailure === 'function') {
-                        action.onFailure(_context.t0);
-                    }
+					if (typeof action.onFailure === 'function') {
+						action.onFailure(_context.t0);
+					}
 
-                    if (!(typeof failedCallback === 'function')) {
-                        _context.next = 37;
-                        break;
-                    }
+					if (!(typeof failedCallback === 'function')) {
+						_context.next = 37;
+						break;
+					}
 
-                    _context.next = 37;
-                    return (0, _effects.call)(failedCallback, errorModel);
+					_context.next = 37;
+					return (0, _effects.call)(failedCallback, errorModel);
 
-                case 37:
-                    if (!(!preventFailedAction && action.preventFailure)) {
-                        _context.next = 40;
-                        break;
-                    }
+				case 37:
+					if (!(!preventFailedAction && !action.preventFailure)) {
+						_context.next = 40;
+						break;
+					}
 
-                    _context.next = 40;
-                    return (0, _effects.put)(errorModel);
+					_context.next = 40;
+					return (0, _effects.put)(errorModel);
 
-                case 40:
-                    _context.next = 43;
-                    break;
+				case 40:
+					_context.next = 43;
+					break;
 
-                case 42:
-                    throw new Error('Api method: [' + action.type + ']() isn\'t defined. Please, create it! Or use another name of action!');
+				case 42:
+					throw new Error('Api method: [' + action.type + ']() isn\'t defined. Please, create it! Or use another name of action!');
 
-                case 43:
-                case 'end':
-                    return _context.stop();
-            }
-        }
-    }, _marked, this, [[11, 30]]);
+				case 43:
+				case 'end':
+					return _context.stop();
+			}
+		}
+	}, _marked, this, [[11, 30]]);
 }
 
 /**
@@ -230,37 +230,37 @@ function callApi(action, apiMethods, options) {
  */
 
 function apiWatchRequest(authTokenSelector) {
-    return _regenerator2.default.wrap(function apiWatchRequest$(_context2) {
-        while (1) {
-            switch (_context2.prev = _context2.next) {
-                case 0:
-                    _context2.next = 2;
-                    return (0, _effects.takeEvery)(function (action) {
-                        return (/^.*_REQUEST$/.test(action.type) && apiRoutes.routes[action.type]
-                        );
-                    }, function (actions) {
-                        return callApi(actions, apiRoutes.routes, authTokenSelector);
-                    });
+	return _regenerator2.default.wrap(function apiWatchRequest$(_context2) {
+		while (1) {
+			switch (_context2.prev = _context2.next) {
+				case 0:
+					_context2.next = 2;
+					return (0, _effects.takeEvery)(function (action) {
+						return (/^.*_REQUEST$/.test(action.type) && apiRoutes.routes[action.type]
+						);
+					}, function (actions) {
+						return callApi(actions, apiRoutes.routes, authTokenSelector);
+					});
 
-                case 2:
-                case 'end':
-                    return _context2.stop();
-            }
-        }
-    }, _marked2, this);
+				case 2:
+				case 'end':
+					return _context2.stop();
+			}
+		}
+	}, _marked2, this);
 }
 ;
 
 var _temp = function () {
-    if (typeof __REACT_HOT_LOADER__ === 'undefined') {
-        return;
-    }
+	if (typeof __REACT_HOT_LOADER__ === 'undefined') {
+		return;
+	}
 
-    __REACT_HOT_LOADER__.register(apiRoutes, 'apiRoutes', 'src/modules/apiWatchRequest.js');
+	__REACT_HOT_LOADER__.register(apiRoutes, 'apiRoutes', 'src/modules/apiWatchRequest.js');
 
-    __REACT_HOT_LOADER__.register(callApi, 'callApi', 'src/modules/apiWatchRequest.js');
+	__REACT_HOT_LOADER__.register(callApi, 'callApi', 'src/modules/apiWatchRequest.js');
 
-    __REACT_HOT_LOADER__.register(apiWatchRequest, 'apiWatchRequest', 'src/modules/apiWatchRequest.js');
+	__REACT_HOT_LOADER__.register(apiWatchRequest, 'apiWatchRequest', 'src/modules/apiWatchRequest.js');
 }();
 
 ;
